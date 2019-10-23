ed3e3385228278872751d9fb38fc1a64c3ff90cb
lgaBug <972514286@qq.com>
2019-10-24 00:17:13 +0800

    ioTest操作

--- ioTest/src/com/lga/io/ClientIoMain02.java
+++ ioTest/src/com/lga/io/ClientIoMain02.java
@@ -1,18 +1,16 @@
 package com.lga.io;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
 import java.net.Socket;
 
-public class ClientIoMain {
+public class ClientIoMain02 {
 
     public static void main(String[] args) throws IOException {
 
         Socket socket = new Socket("127.0.0.1",8001);
 
 
-        socket.getOutputStream().write("hello".getBytes());
+        socket.getOutputStream().write("2222222222222".getBytes());
 
     }
 }
--- ioTest/src/com/lga/io/ServerIoMain.java
+++ ioTest/src/com/lga/io/ServerIoMain.java
@@ -1,4 +1,29 @@
 package com.lga.io;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerIoMain {
+
+    static  byte[] bytes = new byte[1024];
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(8001);
+
+        System.out.println("服务器已经启动。。。。。");
+        while (true){
+            Socket accept = serverSocket.accept();
+
+            System.out.println("客户端链接服务器");
+
+            int read = accept.getInputStream().read(bytes);
+
+            System.out.println("接收到了客户端发来的消息"+new String(bytes));
+
+        }
+
+
+
+    }
 }
