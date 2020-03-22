4c728f2040a0ba6d95e3ed71dac709549da3b8d0
lgaBug <18270234907@163.com>
2020-03-22 20:22:51 +0800

    add xmlRPC

--- xmlRPC/tomcat-server/src/main/java/com/lga/handler/MethodHandler.java
+++ xmlRPC/tomcat-server/src/main/java/com/lga/handler/MethodHandler.java
@@ -3,10 +3,12 @@ package com.lga.handler;
 public class MethodHandler {
 
     public int add(int i1, int i2) {
+        System.out.println("entry add");
         return i1 + i2;
     }
 
     public int sub(int i1, int i2) {
+        System.out.println("entry sub");
         return i1 - i2;
     }
 }
